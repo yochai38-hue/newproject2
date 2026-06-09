9e53f867f4e486e4a085d2a22f3f85f194dc1062
Yochai Besnaino <yochai38@gmail.com>
2026-06-09 19:17:45 +0300

    Implement a basic Dash application in test.py, replacing the print statement with a sample dashboard layout and a line chart.

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print("Hey Yochai")
+import dash
+from dash import html, dcc
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Sample Dashboard'),
+    dcc.Graph(
+        id='sample-graph',
+        figure={
+            'data': [{
+                'x': [1, 2, 3, 4],
+                'y': [10, 15, 13, 17],
+                'type': 'line',
+                'name': 'Sample Data'
+            }],
+            'layout': {
+                'title': 'Sample Line Chart'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
